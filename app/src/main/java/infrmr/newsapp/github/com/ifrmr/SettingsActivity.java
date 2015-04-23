c5f5b9f0885b3e1d5a74486ff374e2154efcd9e5
Jules1991 <julianrosser91@gmail.com>
2015-04-23 16:09:30 +1000

    PreferenceFragment for settings, deleted a few old 'todo' comments

--- app/src/main/java/infrmr/newsapp/github/com/ifrmr/SettingsActivity.java
+++ app/src/main/java/infrmr/newsapp/github/com/ifrmr/SettingsActivity.java
@@ -1,48 +1,17 @@
 package infrmr.newsapp.github.com.ifrmr;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
-
-/**
- * Created by user on 22-Apr-15.
- */
+import android.support.v7.app.ActionBarActivity;
 
 
-
-public class SettingsActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+public class SettingsActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Loads the XML preferences file.
-        addPreferencesFromResource(R.xml.preferences);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        // Registers a callback to be invoked whenever a user changes a preference.
-        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        // Unregisters the listener set in onResume().
-        // It's best practice to unregister listeners when your app isn't using them to cut down on
-        // unnecessary system overhead. You do this in onPause().
-        getPreferenceScreen()
-                .getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
-    }
-
-    // Fires when the user changes a preference.
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        // Sets refreshDisplay to true so that when the user returns to the main
-        // activity, the display refreshes to reflect the new settings.
-        MainActivity.refreshDisplay = true;
+        // Display the settings fragment as the main content.
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
     }
 }
