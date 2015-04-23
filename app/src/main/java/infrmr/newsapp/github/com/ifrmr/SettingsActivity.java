9d336f0a630930e7bb8eaff50c1238a0b2f06e28
Kevin Desai <kevindesai777@gmail.com>
2015-04-23 20:59:53 +0530

    Updated according to the Support library 22.1.0
    Replaced deprecated ActionBarActivity with AppCompatActivity

--- app/src/main/java/infrmr/newsapp/github/com/ifrmr/SettingsActivity.java
+++ app/src/main/java/infrmr/newsapp/github/com/ifrmr/SettingsActivity.java
@@ -1,10 +1,10 @@
 package infrmr.newsapp.github.com.ifrmr;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 
 
-public class SettingsActivity extends ActionBarActivity {
+public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
